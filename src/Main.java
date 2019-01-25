68855c7c3e51017d2cae49e5f6a8486d2f420793
wangyujun <358317180@qq.com>
2019-01-25 08:40:53 +0800

    修改完成

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
-import org.junit.Test;
+
 
 public class Main {
 
-    @Test
-    public void testHelloWrold() {
+    public static void main(String[] args) {
         System.out.println("HelloWorld!");
+        System.out.println("呜呜呜呜");
+        System.out.println("123");
+
+
     }
 }
